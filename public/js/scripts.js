bc9e64a09eb3528d15750abba7c46252d9033fd6
Aadil Glasswala <aglasswala@gmail.com>
2019-02-09 22:37:54 -0500

    fixed the navbar

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,31 +1,55 @@
-$(function() {
-  //run when the DOM is ready
-  $("#ContactUsBtn").click(function() {
-    //use a class, since your ID gets mangled
-    $("#contactform").addClass("active"); //add the class to the clicked element
-  });
+// (function ($) {
+// 	"use strict"; // Start of use strict
 
-  $("#cancel").click(function() {
-    //use a class, since your ID gets mangled
-    $("#contactform").removeClass("active"); //add the class to the clicked element
-  });
-  
-  toggleItem(".button");
-});
+// 	// Smooth scrolling using jQuery easing
+// 	$('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
+// 		if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+// 			var target = $(this.hash);
+// 			target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+// 			if (target.length) {
+// 				$('html, body').animate({
+// 					scrollTop: (target.offset().top - 70)
+// 				}, 1000, "easeInOutExpo");
+// 				return false;
+// 			}
+// 		}
+// 	});
 
-function toggleItem(elem) {
-	$(elem).each(function(){
-		$(this).on("click", function(){
-			var myTarget = $(this).attr("data-target");
-			$(elem).each(function(){
-				var yourTarget = $(this).attr("data-target");
-				if(myTarget !== yourTarget)
-					$(this).removeClass("activeBtn", 300, "easeInBack" );
-			});
-			
-			$(this).toggleClass("activeBtn");
-		});
-	});
+// 	// Closes responsive menu when a scroll trigger link is clicked
+// 	$('.js-scroll-trigger').click(function () {
+// 		$('.navbar-collapse').collapse('hide');
+// 	});
 
-}
+// 	// Activate scrollspy to add active class to navbar items on scroll
+// 	$('body').scrollspy({
+// 		target: '#mainNav',
+// 		offset: 100
+// 	});
 
+// 	// Collapse Navbar
+// 	var navbarCollapse = function () {
+// 		if ($("#mainNav").offset().top > 100) {
+// 			$("#mainNav").addClass("navbar-shrink");
+// 		} else {
+// 			$("#mainNav").removeClass("navbar-shrink");
+// 		}
+// 	};
+// 	// Collapse now if page is not at top
+// 	navbarCollapse();
+// 	// Collapse the navbar when page is scrolled
+// 	$(window).scroll(navbarCollapse);
+
+// })(jQuery); // End of use strict
+
+
+var navbarCollapse = function () {
+	if ($("#mainNav").offset().top > 100) {
+		$("#mainNav").addClass("navbar-shrink");
+	} else {
+		$("#mainNav").removeClass("navbar-shrink");
+	}
+};
+// Collapse now if page is not at top
+navbarCollapse();
+// Collapse the navbar when page is scrolled
+$(window).scroll(navbarCollapse);
