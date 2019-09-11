89dbe293ca0296410bc91be2624b17f62bc68c70
no-vac <crysist14@gmail.com>
2019-09-10 20:34:55 -0400

    maillist button to new list

--- public/js/popup.js
+++ public/js/popup.js
@@ -1,10 +1,22 @@
-window.onload = function(){ 
-
-    function showMailingPopUp() {
-        window.dojoRequire(["mojo/signup-forms/Loader"], function(L) { L.start({"baseUrl":"mc.us19.list-manage.com","uuid":"f25898edc0e8bf966dc8278c9","lid":"dd503b0557","uniqueMethods":true}) });
-    document.cookie = "MCPopupClosed=; path=/; expires=Thu, 01 Jan 1970 00:00:00 UTC";
-    document.cookie = "MCPopupSubscribed=; path=/; expires=Thu, 01 Jan 1970 00:00:00 UTC";
-            };
-    document.getElementById("popup1").onclick = function() {showMailingPopUp()};
-    document.getElementById("popup2").onclick = function() {showMailingPopUp()};  
-    };
+window.onload = function() {
+  function showMailingPopUp() {
+    window.dojoRequire(["mojo/signup-forms/Loader"], function(L) {
+      L.start({
+        baseUrl: "mc.us4.list-manage.com",
+        uuid: "50cee86becc42f2aba98af2e8",
+        lid: "3d7d52cfc4",
+        uniqueMethods: true
+      });
+    });
+    document.cookie =
+      "MCPopupClosed=; path=/; expires=Thu, 01 Jan 1970 00:00:00 UTC";
+    document.cookie =
+      "MCPopupSubscribed=; path=/; expires=Thu, 01 Jan 1970 00:00:00 UTC";
+  }
+  document.getElementById("popup1").onclick = function() {
+    showMailingPopUp();
+  };
+  document.getElementById("popup2").onclick = function() {
+    showMailingPopUp();
+  };
+};
